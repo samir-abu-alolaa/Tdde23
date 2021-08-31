4ee63beea0f5b5dd8e7f1054baed34cb85d36abb
Jonas Kvarnström <jonas.kvarnstrom@liu.se>
2021-08-31 14:48:01 +0200

    Updates

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,4 +1,4 @@
 # Här kan du skriva programkod för labb 2.
 # Du behöver strikt sett inte lämna in koden för denna labb,
 # men vi lägger ändå in en förberedd fil för att förenkla.
-
+# Om du har skrivit labbkoden någon annanstans är det också OK.
